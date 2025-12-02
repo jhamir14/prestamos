9087c0e133d671ebba204fa3b90dcbfddd595093
jhamir14 <jhamirquispe2007@gmail.com>
2025-12-02 14:07:38 -0500

    react

--- gestion_prestamos/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    import pymysql
-    pymysql.install_as_MySQLdb()
-except Exception:
-    # Si PyMySQL no está instalado aún, la importación fallará.
-    # La aplicación seguirá cargando, pero el motor MySQL requerirá
-    # que PyMySQL esté presente para funcionar.
-    pass
