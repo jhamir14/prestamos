3703e4ae34e02423f37f05bb85bdf9550e87e663
jhamir14 <jhamirquispe2007@gmail.com>
2025-12-11 23:44:57 -0500

    IMAGENES

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,8 +20,7 @@ from django.urls import path
 from django.contrib import admin
 from django.urls import path, include
 
-from django.urls import path, include, re_path
-from django.views.static import serve
+from django.urls import path, include
 from django.http import HttpResponse
 
 def health_check(request):
@@ -50,8 +49,4 @@ urlpatterns = [
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += [
-    re_path(r'^media/(?P<path>.*)$', serve, {
-        'document_root': settings.MEDIA_ROOT,
-    }),
-]
+
