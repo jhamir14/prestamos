f8488b1006bee6f7831d282925ee4c6f1b3072df
jhamir14 <jhamirquispe2007@gmail.com>
2025-12-11 23:38:03 -0500

    IMAGENES

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -174,6 +174,7 @@ STATIC_URL = 'static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    WHITENOISE_MANIFEST_STRICT = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
